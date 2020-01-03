425ef189743d6c414e10d341aacf947e23163d5a
Kimyechan <vlvkcjswo7@naver.com>
2020-01-03 16:51:55 +0900

    stack

---
+++ stackPractice/exercise.py
@@ -0,0 +1,16 @@
+#연습1: 리스트 변수로 스택을 다루는 pop, push 기능 구현해보기 (pop, push 함수 사용하지 않고 직접 구현해보기)
+
+stack_list = list()
+
+def push(data):
+	stack_list.append(data)
+
+def pop():
+	data = stack_list[-1]
+	del stack_list[-1]
+	return data
+
+for index in range(10):
+	push(index)
+
+print(pop())
