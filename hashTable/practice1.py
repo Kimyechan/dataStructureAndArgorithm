bc42dbd0e3096252c64a3f1b8e1f721528c8107c
Kimyechan <vlvkcjswo7@naver.com>
2020-01-09 20:46:08 +0900

    hash table

---
+++ hashTable/practice1.py
@@ -0,0 +1,23 @@
+#리스트 변수를 활용해서 해쉬 테이블 구현해보기
+
+hash_table = list([0 for i in range(8)])
+
+def get_key(data):
+	return hash(data)
+
+def hash_function(key):
+	return key % 8
+
+def save_data(data, value):
+	hash_address = hash_function(get_key(data))
+	hash_table[hash_address] = value
+
+def read_data(data):
+	hash_address = hash_function(get_key(data))
+	return hash_table[hash_address]
+
+save_data('Dave', '1111111')
+save_data('Andy', '2222222')
+print(read_data('Dave'))
+
+print(hash_table)
