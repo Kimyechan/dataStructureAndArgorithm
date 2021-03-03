c22326be784e0258c77e67b7c303db9caae46650
Kimyechan <vlvkcjswo7@naver.com>
2021-03-03 23:24:05 +0900

    selection sort 복습

--- concept/sort/selectionsort.py
+++ concept/sort/selectionsort.py
@@ -1,7 +1,23 @@
+# def selectionSort(data):
+#     for stand in range(len(data) - 1):
+#         lowest = stand
+#         for index in range(stand+1, len(data)-1):
+#             if data[lowest] > data[index]:
+#                 lowest = index
+#         data[stand], data[lowest] = data[lowest], data[stand]
+#
+#     return data
+#
+# import random
+#
+# data = random.sample(range(100), 10)
+# print(selectionSort(data))
+
+
 def selectionSort(data):
     for stand in range(len(data) - 1):
         lowest = stand
-        for index in range(stand+1, len(data)-1):
+        for index in range(stand + 1, len(data) - 1):
             if data[lowest] > data[index]:
                 lowest = index
         data[stand], data[lowest] = data[lowest], data[stand]
@@ -10,5 +26,5 @@ def selectionSort(data):
 
 import random
 
-data = random.sample(range(100), 10)
-print(selectionSort(data))
+data_list = random.sample(range(100), 50)
+print(selectionSort(data_list))
