8061d53d6a8e4303d24744de4006df797efdb9e6
Kimyechan <vlvkcjswo7@naver.com>
2021-03-04 00:42:55 +0900

    insertion sort 복습

--- concept/sort/insertionsort.py
+++ concept/sort/insertionsort.py
@@ -1,12 +1,27 @@
+# def insertionSort(data):
+#     for index in range(len(data)-1):
+#         for index2 in range(index+1, 0, -1):
+#             if data[index2] < data[index2-1]:
+#                 data[index2], data[index2-1] = data[index2-1], data[index2]
+#             else:
+#                 break
+#     return data
+#
+# import random
+# data_list = random.sample(range(100), 10)
+# print(insertionSort(data_list))
+
+
 def insertionSort(data):
-    for index in range(len(data)-1):
-        for index2 in range(index+1, 0, -1):
-            if data[index2] < data[index2-1]:
-                data[index2], data[index2-1] = data[index2-1], data[index2]
+    for i1 in range(len(data) - 1):
+        for i2 in range(i1 + 1, 0, -1):
+            if data[i2] < data[i2 - 1]:
+                data[i2], data[i2 - 1] = data[i2 - 1], data[i2]
             else:
                 break
     return data
 
 import random
-data_list = random.sample(range(100), 10)
+
+data_list = random.sample(range(100), 50)
 print(insertionSort(data_list))
