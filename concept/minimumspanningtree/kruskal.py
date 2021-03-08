e04c8dbc89611f40abf6876819debdc9cab9f764
Kimyechan <vlvkcjswo7@naver.com>
2021-03-08 16:20:58 +0900

    kruskal 복습

--- concept/minimumspanningtree/kruskal.py
+++ concept/minimumspanningtree/kruskal.py
@@ -1,42 +1,85 @@
+# parent = dict()
+# rank = dict()
+#
+# def find(node):
+#     if parent[node] != node:
+#         parent[node] = find(parent[node])
+#     return parent[node]
+#
+# def union(node_v, node_u):
+#     root1 = find(node_v)
+#     root2 = find(node_u)
+#
+#     if rank[root1] > rank[root2]:
+#         parent[root2] = root1
+#     else:
+#         parent[root1] = root2
+#         if rank[root1] == rank[root2]:
+#             rank[root2] += 1
+#
+# def make_set(node):
+#     parent[node] = node
+#     rank[node] = 0
+#
+# def kruskal(graph):
+#     mst = list()
+#
+#     for node in graph['vertices']:
+#         make_set(node)
+#
+#     edges = graph['edges']
+#     edges.sort()
+#
+#     for edge in edges:
+#         weight, node_v, node_u = edge
+#         if find(node_v) != find(node_u):
+#             union(node_v, node_u)
+#             mst.append(edge)
+#
+#     return mst
+
 parent = dict()
 rank = dict()
+
 
 def find(node):
     if parent[node] != node:
         parent[node] = find(parent[node])
     return parent[node]
 
-def union(node_v, node_u):
-    root1 = find(node_v)
-    root2 = find(node_u)
 
-    if rank[root1] > rank[root2]:
-        parent[root2] = root1
+def union(v, u):
+    parent1 = find(v)
+    parent2 = find(u)
+
+    if rank[parent1] > rank[parent2]:
+        parent[parent2] = parent1
     else:
-        parent[root1] = root2
-        if rank[root1] == rank[root2]:
-            rank[root2] += 1
+        parent[parent1] = parent2
+        if rank[parent1] == rank[parent2]:
+            rank[parent2] += 1
 
-def make_set(node):
+
+def makeSet(node):
     parent[node] = node
     rank[node] = 0
+
 
 def kruskal(graph):
     mst = list()
 
     for node in graph['vertices']:
-        make_set(node)
+        makeSet(node)
 
-    edges = graph['edges']
-    edges.sort()
+    graph['edges'].sort()
 
-    for edge in edges:
-        weight, node_v, node_u = edge
-        if find(node_v) != find(node_u):
-            union(node_v, node_u)
-            mst.append(edge)
+    for weight, nodeV, nodeU in graph['edges']:
+        if find(nodeV) != find(nodeU):
+            union(nodeV, nodeU)
+            mst.append([weight, nodeV, nodeU])
 
     return mst
+
 
 mygraph = {
     'vertices': ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
