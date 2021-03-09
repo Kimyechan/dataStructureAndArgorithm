c9f8f614621aee9e236ffef20e5e563b37bab0b3
Kimyechan <vlvkcjswo7@naver.com>
2021-03-09 22:18:55 +0900

    back tracking 복습

--- concept/backtracking/queen.py
+++ concept/backtracking/queen.py
@@ -1,24 +1,54 @@
-def is_available(candidate, current_col):
-    current_row =  len(candidate)
-    for queen_row in range(current_row):
-        if candidate[queen_row] == current_col or abs(candidate[queen_row] - current_col) == current_row - queen_row:
+# def is_available(candidate, current_col):
+#     current_row =  len(candidate)
+#     for queen_row in range(current_row):
+#         if candidate[queen_row] == current_col or abs(candidate[queen_row] - current_col) == current_row - queen_row:
+#             return False
+#     return True
+#
+# def DFS(N, current_row, current_candidate, final_result):
+#     if current_row == N:
+#         final_result.append(current_candidate[:])
+#         return
+#
+#     for candidate_col in range(N):
+#         if is_available(current_candidate, candidate_col):
+#             current_candidate.append(candidate_col)
+#             DFS(N, current_row+1, current_candidate, final_result)
+#             current_candidate.pop()
+#
+# def solve_n_queens(N):
+#     final_result = []
+#     DFS(N, 0, [], final_result)
+#     return final_result
+import sys
+
+sys.setrecursionlimit(1000)
+
+def isAvailable(currentCandidate, currentCol):
+    currentTotalRow = len(currentCandidate)
+
+    for currentRow in range(currentTotalRow):
+        if currentCandidate[currentRow] == currentCol or abs(currentCandidate[currentRow] - currentCol) == currentTotalRow - currentRow:
             return False
     return True
 
-def DFS(N, current_row, current_candidate, final_result):
-    if current_row == N:
-        final_result.append(current_candidate[:])
+
+def dfs(N, currentRow, currentCandidate, finalResult):
+    if currentRow == N:
+        finalResult.append(currentCandidate[:])
         return
 
-    for candidate_col in range(N):
-        if is_available(current_candidate, candidate_col):
-            current_candidate.append(candidate_col)
-            DFS(N, current_row+1, current_candidate, final_result)
-            current_candidate.pop()
+    for currentCol in range(N):
+        if isAvailable(currentCandidate, currentCol):
+            currentCandidate.append(currentCol)
+            dfs(N, currentRow + 1, currentCandidate, finalResult)
+            currentCandidate.pop()
 
-def solve_n_queens(N):
-    final_result = []
-    DFS(N, 0, [], final_result)
-    return final_result
 
-print(solve_n_queens(4))
+def solveNQueens(N):
+    finalResult = []
+    dfs(N, 0, [], finalResult)
+    return finalResult
+
+
+print(solveNQueens(10))
